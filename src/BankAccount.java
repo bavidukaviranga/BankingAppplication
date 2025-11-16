fc16280c5ea0b50114d5175359745f850e14e32c
Bavindu Kaviranga <bavindukaviranga2020@gmail.com>
2025-11-16 15:23:24 +0530

    Conditionals in Practice

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -33,4 +33,32 @@ public class BankAccount {
     public int getBalance() {
         return this.balance;
     }
+    public double getTotalValueAfterForAPeriod(int period){
+        if (this.accountType.equals("sa")){
+            if (period > 0){
+                double totalValue = this.balance + this.balance*0.02*period;
+                return totalValue;
+            }else {
+                System.out.println("Wrong period");
+                return this.balance;
+            }
+        }else if(this.accountType.equals("FD")){
+            if (period<=0){
+                System.out.println("Wrong period");
+               return this.balance;
+            }
+            if (period<=2){
+                double totalValue = this.balance + this.balance*0.125*period;
+                return totalValue;
+            } else if (period<=5) {
+                double totalValue = this.balance + this.balance*0.125*period;
+                return totalValue;
+
+            }else{
+                double totalValue = this.balance + this.balance*0.2*period;
+                return totalValue;
+            }
+        }
+        return this.balance;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,16 @@ public class Main {
     public static void main(String[] args) {
         AccountHolder jane =new AccountHolder("Jane Anderson","06/05/1998","22/4,New York");
         BankAccount janesBankAccount = new BankAccount("Jane Anderson","06/05/1998","22/4,New York","4546464","sa");
-        janesBankAccount.depositCash(5000);
-        janesBankAccount.withdrawCash(4000);
-        janesBankAccount.withdrawCash(2000);
+        BankAccount janesBankAccount2 = new BankAccount("Jane Anderson","06/05/1998","22/4,New York","4546465","FD");
+
+
+        janesBankAccount.depositCash(1400);
+        janesBankAccount.withdrawCash(400);
+        janesBankAccount2.depositCash(1000);
+
+        System.out.println(janesBankAccount.getBalance());
+        System.out.println(janesBankAccount.getTotalValueAfterForAPeriod(7));
+
+        System.out.println(janesBankAccount2.getTotalValueAfterForAPeriod(4));
     }
 }
